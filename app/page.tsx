a35b94a484915bb274dd3787449e8a802c6014bf
teg-hub <basette@theempowergroup.org>
2025-10-31 20:25:02 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
+// app/page.tsx
 import Link from 'next/link';
 export default function Home() {
   return (
     <main className="container">
       <h1>Welcome to Fair Chance</h1>
-      <p>Proceed to <Link href="/auth/login">Login</Link></p>
+      <p><Link href="/auth/login">Go to Login</Link></p>
     </main>
   );
 }
