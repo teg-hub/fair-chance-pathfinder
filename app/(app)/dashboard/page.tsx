8f442528dd4076b1ca1f50e13ab11e258c683036
teg-hub <basette@theempowergroup.org>
2025-11-03 17:50:58 -0500

    Delete app/(app)/dashboard directory

--- app/(app)/dashboard/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Dashboard() {
-  return (
-    <main className="container">
-      <h2>Dashboard</h2>
-      <p>Logged in. KPI widgets will go here.</p>
-    </main>
-  );
-}
